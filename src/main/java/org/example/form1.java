cf367c8db78757651afaeeb72610b41d5329b82d
Dennis <dennis.diaz@epn.edu.ec>
2024-06-21 11:43:02 -0500

    CODIFICACION 2

--- src/main/java/org/example/form1.java
+++ src/main/java/org/example/form1.java
@@ -1,8 +1,27 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class form1 {
     public JPanel mainPanel;
-    private JButton button1;
+    private JButton OKButton;
+    private JLabel Jlabel;
+    private JButton Borrarjl;
+
+    public form1() {
+        OKButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Jlabel.setText("Hola a todos, soy Dennis Díaz");
+            }
+        });
+        Borrarjl.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Jlabel.setText(" ");
+            }
+        });
+    }
 }
